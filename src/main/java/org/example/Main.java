52fe8cec338ac1ec08464129b44dc04a1f411f5d
wesley <wesleyemidio99@hotmail.com>
2023-07-08 16:45:33 -0300

    Confirmacao de codigo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
         Scanner valor = new Scanner(System.in);
 
-        int alcool=0,gasolina=0,diesel=0,n;
+        int alcool=0,gasolina=0,diesel=0,n,cliente=0;
 
         System.out.println("Digite sua preferencia de combustivel");
         System.out.println("1 - Alcool \n2 - Gasolina\n3 - Diesel\n4 - Resultados");
@@ -15,24 +15,32 @@ public class Main {
 
         while(n != 4){
 
-            if(n ==1){
-                alcool += 1;
-            }
-            else if(n == 2){
-                gasolina += 1;
-            }
-            else {
-                diesel += 1;
+            if(n >=1 && n <= 3){
+
+                if(n ==1){
+                    alcool += 1;
+                }
+                else if(n == 2){
+                    gasolina += 1;
+                }
+                else {
+                    diesel += 1;
+
+                }
+
+                System.out.println("Digite sua preferencia de combustivel");
+                System.out.println("1 - Alcool \n2 - Gasolina\n3 - Diesel\n4 - Resultados");
+                n = valor.nextInt();
 
             }
-
-            System.out.println("Digite sua preferencia de combustivel");
-            System.out.println("1 - Alcool \n2 - Gasolina\n3 - Diesel\n4 - Resultados");
-            n = valor.nextInt();
-
+            else{
+                System.out.println("Codigo invalido digite novamente:");
+                System.out.println("1 - Alcool \n2 - Gasolina\n3 - Diesel\n4 - Resultados");
+                n = valor.nextInt();
+            }
         }
 
-        System.out.printf("Os resultados sao:\nAlcool = %d\nGasolina = %d\nDiesel = %d",alcool,gasolina,diesel);
+    System.out.printf("MUITO OBRIGADO\nOs resultados sao:\nAlcool = %d\nGasolina = %d\nDiesel = %d",alcool,gasolina,diesel);
 
     }
 }
